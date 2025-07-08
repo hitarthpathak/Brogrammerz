bf498b308e8aa361fce18f3d4ff83833b5d02f81
Hitarth Pathak <163673363+hitarthpathak@users.noreply.github.com>
2025-07-08 12:09:33 +0530

    Update Profile.js

--- Profile.js
+++ Profile.js
@@ -31,7 +31,7 @@ let logged_in_user = users_data.find((filter_user) => {
 
 if (!logged_in_user) {
     alert("User Is Not Logged In! Redirecting To Login Page.");
-    location = "/";
+    location = "index.html";
 }
 
 // ------------------------------------------------------------------------------------------------
@@ -97,7 +97,7 @@ function logout() {
     localStorage.removeItem("logged-in-user-email");
     localStorage.removeItem("searched-user-email");
     localStorage.removeItem("search-query");
-    location = "/";
+    location = "index.html";
 
 };
 
@@ -115,7 +115,7 @@ function delete_profile() {
         localStorage.removeItem("search-query");
         localStorage.removeItem("searched-user-email");
         alert("Your Profile Is Deleted!");
-        location = "/";
+        location = "index.html";
     }
     else {
         return false;
